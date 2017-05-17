b43016655126a5f7f3874f176c5b969c2f1bff57
Ryan Swann <RyanSwann1992@gmail.com>
2017-05-17 11:10:16 +0100

    Adding missing file.

---
+++ src/RandomNumberGenerator.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <random>
+class RandomNumberGenerator
+{
+public:
+	static int getRandNumb(int min, int max)
+	{
+		std::random_device rd; 
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(min, max);
+
+		return dis(gen);
+	}
+
+private:
+	RandomNumberGenerator();
+
+
+};
